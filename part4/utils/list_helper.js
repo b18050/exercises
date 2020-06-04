d3e1fb42d3039ec209e2b2f759fd723d7c21836b
chandan <b18050@students.iitmandi.ac.in>
2020-06-05 00:28:11 +0530

    intro to testing

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+	const reducer = (sum, blog) => {
+		return sum + blog.likes
+	}
+
+	return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+	const favblog = blogs.reduce((prev,current) => (prev.likes > current.likes) ? prev : current)
+	const result = {
+		title: favblog.title,
+		author: favblog.author,
+		likes: favblog.likes,
+	}
+	console.log(result)
+	return (result);
+}
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
