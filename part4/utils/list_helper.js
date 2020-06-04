a095209753b0ae90f18e8a068500c6d9502035df
chandan <b18050@students.iitmandi.ac.in>
2020-06-05 01:39:48 +0530

    added eslint

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,28 +1,22 @@
-const dummy = (blogs) => {
-  return 1;
-}
-
 const totalLikes = (blogs) => {
-	const reducer = (sum, blog) => {
-		return sum + blog.likes
-	}
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
 
-	return blogs.reduce(reducer, 0)
+  return blogs.reduce(reducer, 0)
 }
 
 const favoriteBlog = (blogs) => {
 
-	const favblog = blogs.reduce((prev,current) => (prev.likes > current.likes) ? prev : current)
-	const result = {
-		title: favblog.title,
-		author: favblog.author,
-		likes: favblog.likes,
-	}
-	console.log(result)
-	return (result);
+  const favblog = blogs.reduce((prev,current) => (prev.likes > current.likes) ? prev : current)
+  const result = {
+    title: favblog.title,
+    author: favblog.author,
+    likes: favblog.likes,
+  }
+  return (result)
 }
 module.exports = {
-  dummy,
   totalLikes,
   favoriteBlog
 }
