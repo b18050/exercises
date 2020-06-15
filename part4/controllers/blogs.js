2665276cd475e70c1510d24fd13f9dc26bca56d3
chandan <b18050@students.iitmandi.ac.in>
2020-06-15 16:47:42 +0530

    test expansion step4

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,15 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
 
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name :1 })
   response.json(blogs.map(blog => blog.toJSON()))
     
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
+
   if(!body.title || !body.url){
     response.status(400).json({
       error: 'title or url missing'
@@ -20,30 +23,34 @@ blogsRouter.post('/', async (request, response) => {
       title: body.title,
       author: body.author,
       url : body.url,
-      likes: body.likes
+      likes: body.likes,
+      user: user._id
     })
     
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.json(savedBlog.toJSON())
   }
 })
 
-  blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
     
-    await Blog.findByIdAndRemove(request.params.id)
+  await Blog.findByIdAndRemove(request.params.id)
 
-    response.status(204).end()
+  response.status(204).end()
 
-  })
+})
   
-  blogsRouter.put('/:id', async (request, response) => {
-    const body = request.body
-    const blog = {
-      likes: body.likes
-    }
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new : true})
-    response.json(updatedBlog)
-  })
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new : true})
+  response.json(updatedBlog)
+})
   
   
 
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -9,7 +9,7 @@ const blogSchema = new mongoose.Schema({
   likes: {
     type: Number,
     default: 0
-  }
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
