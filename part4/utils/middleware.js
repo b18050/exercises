79d0aa72ee7113a2eb013ca0a36e3e020e0fff19
chandan <b18050@students.iitmandi.ac.in>
2020-06-15 17:43:05 +0530

    bloglist expansion step 9

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,10 +1,21 @@
 const logger = require('./logger')
+const { request } = require('../app')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
   logger.info('---')
+  next()
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  else
+    return null
   next()
 }
 
