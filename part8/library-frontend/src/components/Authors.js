5d749051b1a86eaad2f3bae39717c8a31b60350c
b18050 <abchandan11@gmail.com>
2021-02-06 02:15:15 +0530

    8.12: Authors birth year advanced

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,5 +1,6 @@
 import React, { useState }  from 'react'
 import { gql, useQuery,useMutation } from '@apollo/client';
+import Select from 'react-select';
 
 const ALL_AUTHORS = gql`
 query {
@@ -27,11 +28,13 @@ const Authors = (props) => {
   
   const result = useQuery(ALL_AUTHORS);
 
-  const [name, setName] = useState('')
+  // const [name, setName] = useState('')
+  const [selectedOption, setSelectedOption] = useState('')
   const [born, setBorn] = useState('')
 
   const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
-    refetchQueries: [ { query: ALL_AUTHORS } ]
+    refetchQueries: [ { query: 
+      ALL_AUTHORS } ]
   })
   if (!props.show) {
     return null
@@ -39,17 +42,23 @@ const Authors = (props) => {
   if(result.loading){
     return <div>loading...</div>
   }
-
+  const options = result.data.allAuthors.map(a => ({
+    value: String(a.name),
+    label: String(a.name)
+  }))
+  console.log(options)
   const submit = (event) => {
     event.preventDefault()
-    
+    const name = String(selectedOption.value)
+    console.log(selectedOption)
     editAuthor({  variables: { name, born } })
     console.log('edit author...')
 
-    setName('')
+    // setName('')
     setBorn('')
 
   }
+
   
   return (
     <div>
@@ -81,9 +90,14 @@ const Authors = (props) => {
           <form onSubmit={submit}>
             <div>
               name
-              <input
+              {/* <input
                 value={name}
                 onChange={({ target }) => setName(target.value)}
+              /> */}
+              <Select
+                defaultValue={selectedOption}
+                onChange={setSelectedOption}
+                options={options}
               />
             </div>
             <div>
