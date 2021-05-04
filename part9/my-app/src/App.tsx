8121d24ad1f1ff83bf46caff144b02c65d3c484b
b18050 <abchandan11@gmail.com>
2021-05-05 01:52:50 +0530

    Added types

--- part9/my-app/src/App.tsx
+++ part9/my-app/src/App.tsx
@@ -1,50 +1,105 @@
 import React from 'react';
-
-interface course {
-  name: string,
-  exerciseCount: number
-}
-
+import { parseJsonText } from 'typescript';
+import {CoursePart} from './types'
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
-  const Header = ( {courseName}:{ courseName:string}) => 
-    (
+const Header = ( {courseName}:{ courseName:string}) => (
       <>
         <h1>{courseName}</h1>
       </>
-    );
-
-const Content = ({courseParts}: {courseParts:course[]}) => (
-  <>
-    <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-  </>
 );
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+const Part = ({part}: {part:CoursePart }) => {
+    switch(part.type){
+      case "normal":
+        return(
+          <>
+          <p><b>{part.name} {part.exerciseCount} </b><br></br>{part.description} </p>
+          </>
+        )
+      case "groupProject":
+        return(
+          <>
+          <p><b> {part.name} {part.exerciseCount}</b>
+          <br></br>project exercises {part.groupProjectCount}</p>
+          </>
+        )
+        
+      case "submission":
+        return (
+          <>
+          <p> <b>{part.name} {part.exerciseCount}</b><br></br><i> {part.description}</i>
+          <br></br> submit to {part.exerciseSubmissionLink}</p>
+          </>
+        )
+        
+      case "special":
+        return (
+          <>
+          <p><b>{part.name} {part.exerciseCount} </b>
+          <br></br>{part.description} <br></br>
+          required skills:{part.requirements.map (r => (r+' '))}</p>
+          </>
+        )
+        
+      default:
+        return assertNever(part);
+    }
+};
+
+const Content = ({courseParts}: {courseParts: CoursePart[]}) => {
+  return (
+    <>
+      
+      {courseParts.map((part) => (<Part key={part.name} part={part} />))}  
+      
+    </>
+  );
+};
   
-const Total = ({courseParts}: {courseParts:course[]}) => (
+const Total = ({courseParts}: {courseParts: CoursePart[]}) => (
   <>
     <p>
         Number of exercises{" "}
---
+++ part9/my-app/src/types.ts
@@ -0,0 +1,35 @@
+
+export interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+  
+  interface CourseNormalSubmissionPart extends CoursePartBase {
+    description: string;
+  }
+  
+  interface CourseNormalPart extends CourseNormalSubmissionPart {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+  
+  interface CourseSubmissionPart extends CourseNormalSubmissionPart {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+  
+  interface CourseRequirement extends CourseNormalSubmissionPart {
+    type: "special";
+    requirements: string[];
+  }
+  
+export type CoursePart = 
+        CourseNormalPart | 
+       CourseProjectPart | 
+    CourseSubmissionPart | 
+        CourseRequirement;
+  
